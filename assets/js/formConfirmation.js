66a39885c4e00e76e470c6d22a406c498271037e
thot <135855332+thotcode@users.noreply.github.com>
2024-11-17 20:09:10 -0300

    fix re

--- assets/js/formConfirmation.js
+++ assets/js/formConfirmation.js
@@ -3,7 +3,7 @@ function ValidateEmail(email) {
 		alert("spam detected.");
 		return false;
 	}
-	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/.test(email)) {
+	if (/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
 		return (true)
 	}
 	alert(email + " is an invalid email address! Plese insert a valid one.");
